0252a2e2d59c91649652d3dc692c296d077fcff2
Andrea Biemmi <106437692+BmmNdr@users.noreply.github.com>
2023-11-23 13:27:45 +0100

    Client comunication logic

--- Client/app.java
+++ Client/app.java
@@ -1,3 +1,4 @@
+
 // app.java
 import java.io.IOException;
 
@@ -6,12 +7,20 @@ public class app {
         GUI gui = new GUI();
         GameClient gameClient = new GameClient(gui);
 
+        // TODO riceve un messaggio dal server, poi capisce cosa fare (se muovere le
+        // palline, attendere o inviare una mossa)...
 
-        //TODO riceve un messaggio dal server, poi capisce cosa fare (se muovere le palline, attendere o inviare una mossa)...
-        
         while (true) {
-            String tmp = gameClient.in.readLine();
-            gameClient.receiveGameState(tmp); //TODO ...quindi al receiveGameState invio una stringa con le palline in CSV
+            String[] tmp = gameClient.in.readLine().split(";", 2);
+
+            if (tmp[0].equals("paint"))
+                gameClient.receiveGameState(tmp[1]);
+            else if (tmp[0].equals("turn"))
+
+            else if (tmp[0].equals("wait"))
+
+            else if (tmp[0].equals("end"))
+
         }
     }
 }
